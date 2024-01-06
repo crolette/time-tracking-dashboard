7fd88551d0c233b999a6e53a8c90b60fe318d23c
Crolweb <crolweb@gmail.com>
2024-01-06 14:25:34 +0100

    commit develop branch

---
+++ app.js
@@ -0,0 +1,8 @@
+let cards = document.getElementsByClassName("dashboard__card");
+let choices = document.getElementsByClassName("choice__select");
+console.log(cards);
+console.log(choices);
+
+for (const choice of choices) {
+  console.log(choice);
+}
